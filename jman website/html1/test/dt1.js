c7b1279e096cf6d2728edd02e9c3cb75cdc1cdb0
JMAN <lambejohn6@gmail.com>
2022-06-30 02:47:21 +0100

    committed

---
+++ jman website/html1/test/dt1.js
@@ -0,0 +1,16 @@
+// bolean/logical type
+let hasphone = false
+let hashouse = true
+let b = 5 > 7
+console.log(b)
+
+
+//the "null" value
+let amount = null
+
+//the "undefined"value
+let salary
+console.log(salary)
+
+
+console.log(typeof b)
